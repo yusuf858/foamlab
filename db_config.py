f0f40630d22cb0aa2a7a261fdad1d1b1f52b8e1b
Mohammed Yusuf <mohdyusuf4106@gmail.com>
2026-03-10 22:35:34 +0530

    connect filess.io database

--- db_config.py
+++ db_config.py
@@ -2,8 +2,8 @@
 # Edit this file with YOUR MySQL password and save it.
 # Place this file inside the FoamLab/ folder (same folder as app.py)
 
-DB_HOST = "localhost"
+DB_HOST = "tsdhnq.h.filess.io"
 DB_PORT = 3306
-DB_USER = "root"
-DB_PASS = ""        # <-- PUT YOUR MYSQL ROOT PASSWORD HERE (leave "" if no password)
-DB_NAME = "foamlab_db"
+DB_USER = "foamlab_db_bodywestmy"
+DB_PASS = "56b6ea2000c916dfb28f08b458bc72f6d39f1f3e"        # <-- PUT YOUR MYSQL ROOT PASSWORD HERE (leave "" if no password)
+DB_NAME = "foamlab_db_bodywestmy"
